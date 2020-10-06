346094671fd5fba66db8aebf0fba3459758d7506
nikitagru <50076302+nikitagru@users.noreply.github.com>
2020-10-06 20:17:45 +0500

    production

--- main.bat
+++ main.bat
@@ -1,1 +1,25 @@
 @echo off
+SetLocal
+set x=%cd%
+
+REM dir %cd%\* /b/s > filelist.txt
+REM chcp 866> nul
+REM REM for /F "delims=" %%a in (filelist.txt) do (
+REM     chcp 65001> nul 
+REM     echo.88.txt>c:\Users\gruni\Desktop\Projects\DecoderBatt\89.txt
+REM REM )
+for /f "tokens=*" %%i in (dir.txt) do (
+    for /f "tokens=*" %%a in (%%i) do (
+        set x=%%i
+        call :convert %%a
+    ) 
+)
+
+
+ 
+:convert 
+chcp 65001 >nul
+echo %* >> %x%.txt
+chcp 866 >nul
+
+
