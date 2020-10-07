2dbc45c1418d6d75b518aeeb89744b681b93fc38
nikitagru <50076302+nikitagru@users.noreply.github.com>
2020-10-07 19:50:56 +0500

    Rename new files

--- main.bat
+++ main.bat
@@ -2,24 +2,30 @@
 SetLocal
 set x=%cd%
 
-REM dir %cd%\* /b/s > filelist.txt
+if exist %1 (
+    dir %1 /b/s > filelist.txt 2>nul
 REM chcp 866> nul
 REM REM for /F "delims=" %%a in (filelist.txt) do (
 REM     chcp 65001> nul 
 REM     echo.88.txt>c:\Users\gruni\Desktop\Projects\DecoderBatt\89.txt
 REM REM )
-for /f "tokens=*" %%i in (dir.txt) do (
+for /f "tokens=*" %%i in (filelist.txt) do (
     for /f "tokens=*" %%a in (%%i) do (
         set x=%%i
-        call :convert %%a
-    ) 
+        call :convert %%a 2>nul
+    )
+    del %%i
 )
 
 
- 
+) else (
+    echo Error
+)
+
 :convert 
 chcp 65001 >nul
-echo %* >> %x%.txt
+echo %* >> %x%.txt 2>nul
 chcp 866 >nul
 
 
+ren %1\*.txt *. 2>nul
